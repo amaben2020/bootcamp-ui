1fd22ffe3a41a3207a1bc6e5a7f7541054867914
algomachine007 <amaben202phantom@gmail.com>
2022-12-17 11:33:08 +0100

    building single bootcamp page

--- base/services/index.ts
+++ base/services/index.ts
@@ -1,8 +1,17 @@
 import { api } from "../api"
 
-export const fetchSingleBootcamp = async (id: string) => {
+export const fetchSingleBootcamp = async (path: string, id: string) => {
   try {
-    const data = await api.get(id)
+    const data = await api.get(`/${path}/${id}`)
+    return data
+  } catch (error: any) {
+    console.log(error.message)
+  }
+}
+
+export const fetchAllBootcamps = async (path: string) => {
+  try {
+    const data = await api.get(`/${path}`)
     return data
   } catch (error: any) {
     console.log(error.message)
--- pages/bootcamp/[id].tsx
+++ pages/bootcamp/[id].tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { fetchAllBootcamps, fetchSingleBootcamp } from '../../base/services';
 
-const BootCamp = () => {
-  return <div>BootCamp</div>;
+const BootCamp = ({ data }) => {
+  console.log('DATA', data);
+  return (
+    <div>
+      <h2>{data.name}</h2>
+    </div>
+  );
 };
 
 export default BootCamp;
 
-fetchSingleBootcamp;
+export const getStaticProps = async ({ params }: any) => {
+  console.log(params);
+  let bootcamp = {};
+
+  try {
+    const data = await fetchSingleBootcamp('bootcamps', params.id);
+
+    bootcamp = data?.data.bootcamp;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    props: {
+      data: bootcamp,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const bootcamps = (await fetchAllBootcamps('bootcamps'))?.data.data;
+
+  const paths = bootcamps.map((elem: any) => ({
+    params: { id: elem._id.toString() },
+  }));
+
+  console.log('Paths', paths);
+
+  return {
+    paths: paths,
+    fallback: true,
+  };
+};
